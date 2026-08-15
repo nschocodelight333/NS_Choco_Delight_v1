f93793ed5e03e0a2c4da9eb0c9d18a0cd9616f29
nschocodelight333 <komeshbathula@gmail.com>
2026-08-15 09:00:47 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  const uri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/choco-delight';
+  const uri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/choco-delightmongodb+srv://nschocoadmin:XXXX@cluster0.mbwbvuy.mongodb.net/?appName=Cluster0';
   try {
     const conn = await mongoose.connect(uri, { serverSelectionTimeoutMS: 5000 });
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
